19c949f7b7a81b9e60c2757942fdfcabcd71250e
Long17369 <long17368@foxmail.com>
2026-03-18 09:23:43 +0800

    统一接口返回形式

--- src/parse/courseList.py
+++ src/parse/courseList.py
@@ -4,7 +4,9 @@ from yarl import URL
 from base import EducoderSession
 
 
-async def get_origin_course_list(session: EducoderSession, username: str) -> dict[str, Any]:
+async def get_origin_course_list(
+    session: EducoderSession, username: str
+) -> dict[str, Any]:
     """获取课程列表
 
     参数:
@@ -20,7 +22,7 @@ async def get_origin_course_list(session: EducoderSession, username: str) -> dic
         courses: list // 课程列表
     }
     ```
-    courses 列表中的每个元素是一个字典，主要包含以下字段:
+    courses 列表中的每个元素是一个字典, 主要包含以下字段:
     - `first_category_url` 课程一级分类的 URL
     - `name` 课程名称
     """
@@ -33,7 +35,7 @@ async def get_origin_course_list(session: EducoderSession, username: str) -> dic
 
 async def get_course_list(
     session: EducoderSession, username: str
-) -> dict[str, tuple[URL, str]]:
+) -> list[tuple[str, URL, str]]:
     """获取课程 URL 列表
 
     参数:
@@ -41,13 +43,16 @@ async def get_course_list(
     - `username`: 用户名 // 由 `get_user_info` 获取的字典的 `login` 字段
 
     返回值:
-    一个字典，键为课程名称，值为一个元组，包含课程 URL 和 课程uuid
+    一个列表, 值为一个元组, 包含课程名称, 课程 URL 和 课程uuid
     """
     data = await get_origin_course_list(session, username)
-    course_url_list: dict[str, tuple[URL, str]] = {}
+    course_url_list: list[tuple[str, URL, str]] = []
     for course in data["courses"]:
-        course_url_list[course["name"]] = (
-            URL("https://www.educoder.net" + course["first_category_url"]),
-            course["first_category_url"].split("/")[2],
+        course_url_list.append(
+            (
+                course["name"],
+                URL("https://www.educoder.net" + course["first_category_url"]),
+                course["first_category_url"].split("/")[2],
+            )
         )
     return course_url_list
